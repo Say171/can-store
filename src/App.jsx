fa41106a7868ad09ab41bb84d1eaa4101b430495
Say H <chse22036@g.nihon-u.ac.jp>
2023-11-17 19:02:58 +0900

    ****

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 
+// App.jsx
 function App() {
+  // 製品データ、選択されたカテゴリ、検索語、表示される製品_管理
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState("All");
   const [searchTerm, setSearchTerm] = useState("");
@@ -8,23 +10,25 @@ function App() {
   const [lastCategory, setLastCategory] = useState("All");
   const [lastSearch, setLastSearch] = useState("");
 
+  // コンポーネントがマウントされたときに製品データをフェッチする
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("/products.json");
         if (!response.ok) {
-          throw new Error(`HTTP error: ${response.status}`);
+          throw new Error(`HTTPエラー: ${response.status}`);
         }
         const data = await response.json();
         initialize(data);
       } catch (error) {
-        console.error(`Fetch problem: ${error.message}`);
+        console.error(`フェッチの問題: ${error.message}`);
       }
     };
 
     fetchData();
-  }, []); // 空の依存配列を渡して初回のみ呼び出し
+  }, []); // マウント時に1回だけ実行
 
+  // 製品データを初期化する関数
   const initialize = (products) => {
     setProducts(products);
     setLastCategory("All");
@@ -33,19 +37,24 @@ function App() {
     updateDisplay();
   };
 
+  // カテゴリの選択とワードの入力の関数
+  // 大変
   const selectCategory = (e) => {
     e.preventDefault();
 
+    // カテゴリとワードが変更されていない場合は何もしない
     if (category === lastCategory && searchTerm.trim() === lastSearch) {
       return;
     }
 
+    // 最後のカテゴリと検索語を更新
     setLastCategory(category);
     setLastSearch(searchTerm.trim());
 
     let categoryGroup = [];
     let updatedFinalGroup = [];
 
+    // 選択されたカテゴリ：フィルタリング
     if (category === "All") {
       categoryGroup = products;
       updatedFinalGroup = categoryGroup;
@@ -55,6 +64,7 @@ function App() {
       updatedFinalGroup = categoryGroup;
     }
 
+    // ワード：フィルタリング
     if (searchTerm.trim() !== "") {
       const lowerCaseSearchTerm = searchTerm.trim().toLowerCase();
       updatedFinalGroup = categoryGroup.filter((product) =>
@@ -62,25 +72,30 @@ function App() {
       );
     }
 
+    // 更新：フィルタリング
     setFinalGroup(updatedFinalGroup);
     updateDisplay();
   };
 
+  // 表示を更新する関数
   const updateDisplay = () => {
-    // 画面更新のロジックを追加
-    // ...
+    // 拡張要素
   };
 
   return (
     <>
+      {/* ヘッダー*/}
       <header>
         <h1>The Can Store</h1>
       </header>
+      {/* メイン */}
       <div>
+        {/* サイドバー/アサイド */}
         <aside>
           <form>
             <div>
-              <label htmlFor="category">Choose a category:</label>
+              <label htmlFor="category">カテゴリを選択：</label>
+              {/* 製品カテゴリを選択：ドロップダウン */}
               <select
                 id="category"
                 value={category}
@@ -93,25 +108,29 @@ function App() {
               </select>
             </div>
             <div>
-              <label htmlFor="searchTerm">Enter search term:</label>
+              <label htmlFor="searchTerm">検索語を入力：</label>
+              {/* 製品検索：ワード入力フィールド */}
               <input
                 type="text"
                 id="searchTerm"
-                placeholder="e.g. beans"
+                placeholder="例：豆"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
             </div>
             <div>
+              {/* フィルタリングボタン */}
               <button type="button" onClick={selectCategory}>
-                Filter results
+                結果をフィルタリング
               </button>
             </div>
           </form>
         </aside>
+        {/* フィルタリングされた製品を表示*/}
         <main>
+          {/* 製品がない場合のメッセージ表示 */}
           {finalGroup.length === 0 ? (
-            <p>No results to display!</p>
+            <p>表示する結果はありません！</p>
           ) : (
             finalGroup.map((product, index) => (
               <section key={index} className={product.type.toLowerCase()}>
@@ -123,24 +142,26 @@ function App() {
           )}
         </main>
       </div>
+      {/* フッター */}
       <footer>
-        <p>All icons found at the Noun Project:</p>
+        <p>アイコンはすべてNoun Projectで見つかりました：</p>
         <ul>
+          {/* リンク */}
           <li>
-            Bean can icon by{" "}
+            ビーン缶アイコン作成者：
             <a href="https://thenounproject.com/yalanis/">Yazmin Alanis</a>
           </li>
           <li>
-            Vegetable icon by{" "}
+            野菜アイコン作成者：
             <a href="https://thenounproject.com/skatakila/">Ricardo Moreira</a>
           </li>
           <li>
-            Soup icon by{" "}
+            スープアイコン作成者：
             <a href="https://thenounproject.com/ArtZ91/">Arthur Shlain</a>
           </li>
           <li>
-            Meat Chunk icon by{" "}
-            <a href="https://thenounproject.com/smashicons/">Oliviu Stoian</a>.
+            肉の塊アイコン作成者：
+            <a href="https://thenounproject.com/smashicons/">Oliviu Stoian</a>
           </li>
         </ul>
       </footer>
